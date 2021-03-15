644091d3a7bda93734c2c7c9f481b47cfa2ea776
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2021-03-15 14:41:59 +0700

    Update Page 404

--- resources/views/emailku.blade.php
+++ resources/views/emailku.blade.php
@@ -1,6 +1,6 @@
-With Button <br>
+Klik URL di bawah ini untuk melakukan reset password : <br>
 <a href="http://localhost:8000/password/reset/{{ $token }}?email=naufalhaidirridha%40rocketmail.com" target="_blank"> Click </a>
 <hr>
 
-Manual <br>
+Atau gunakan manual copy-paste di browser dengan link berikut : <br>
 http://localhost:8000/password/reset/{{ $token }}?email=naufalhaidirridha%40rocketmail.com

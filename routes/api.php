7697d2dc3e3264e7b7e263368135c294484d7845
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2021-03-16 15:11:48 +0800

    Update Api Route

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\User;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
+});
+
+// Dummy
+Route::middleware('auth:api')->get('/dummies', function (Request $request) {
+  return User::all();
 });
